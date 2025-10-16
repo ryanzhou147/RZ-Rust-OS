980b414ad40af393476aa8607db69a68920ed796
Ryan Zhou <ryanawesome888@gmail.com>
2025-10-15 21:26:50 -0700

    Structure for custom Rust OS

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rz_rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use rz_rust_os::println;
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    rz_rust_os::init();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rz_rust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
