595934ba175d757d40ba646820a2a938a925dc74
Ryan <ryanawesome888@gmail.com>
2025-11-02 12:27:22 -0500

    Expose network::init() and network::poll() outside scope

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -8,3 +8,7 @@ pub mod icmp;
 pub mod udp;
 pub mod sockets;
 pub mod checksums;
+pub mod network;
+
+// Callers can use crate::network::init() and crate::network::poll().
+pub use network::{init, poll};
